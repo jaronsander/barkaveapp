4e7f97a191b49b62120b62e0253c2e507a927fb7
jaronsander <jar.sand.on@gmail.com>
2024-03-07 19:51:27 -0800

    fixed caching

--- app/api/getwalkers/route.js
+++ app/api/getwalkers/route.js
@@ -7,9 +7,17 @@ export async function GET(request) {
     const users = await clerkClient.users.getUserList();
     const walker = users.filter(user => user.publicMetadata.role.includes('walker'));
     console.log(walker);
-    return NextResponse.json(walker);
-    } catch (error) {
+    return new NextResponse(JSON.stringify(walker), {
+      headers: {
+        'Cache-Control': 'no-store', // or 'no-cache' depending on your requirements
+      },
+    });
+  } catch (error) {
     console.error('Error fetching users:', error.message);
-    return NextResponse.json([]);
-    }
+    return NextResponse.json([], {
+      headers: {
+        'Cache-Control': 'no-store', // or 'no-cache' depending on your requirements
+      },
+    });
+  }
 }
